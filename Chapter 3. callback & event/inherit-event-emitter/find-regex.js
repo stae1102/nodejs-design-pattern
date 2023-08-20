0b46947fce9f4ca6bf3f7fb2a953ac55116f6cc1
Seongtae <stae1102@gmail.com>
2023-08-20 16:44:07 +0900

    feat: inherit event emitter

---
+++ Chapter 3. callback & event/inherit-event-emitter/find-regex.js
@@ -0,0 +1,33 @@
+import { EventEmitter } from 'events';
+import { readFile } from 'fs';
+
+export class FindRegex extends EventEmitter {
+  constructor(regex) {
+    super();
+    this.regex = regex;
+    this.files = [];
+  }
+
+  addFile(file) {
+    this.files.push(file);
+    return this;
+  }
+
+  find() {
+    for (const file of this.files) {
+      readFile(file, 'utf8', (err, content) => {
+        if (err) {
+          return this.emit('error', err);
+        }
+
+        this.emit('fileread', file);
+        const match = content.match(this.regex);
+        if (match) {
+          match.forEach((elem) => this.emit('found', file, elem));
+        }
+      });
+
+      return this;
+    }
+  }
+}
